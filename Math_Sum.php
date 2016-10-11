dbdb24f000413c562e823e97024524230a88ea0c
mittelmabigkeit <comp422012016@mail.ru>
2016-10-11 16:15:08 +0300

    vot

---
+++ Math_Sum.php
@@ -0,0 +1,8 @@
+<?php
+class Math_Sum {
+	public $a,$b,$res;
+	function sum ($a,$b) {
+		$this -> res=$a+$b;
+	}
+};
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'Math_Sum.php';
+$obj = new Math_Sum;
+$obj -> sum(2,3);
+echo $obj -> res;
+?>
